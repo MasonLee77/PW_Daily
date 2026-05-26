7f16151a72a28c82fdf4fa5e200b7a2e8bff16f9
Mason <mac@MasonLees-MBP.local>
2026-05-27 00:31:26 +0700

    day 2

--- src/tests/selector_automation/register_page.spec.ts
+++ src/tests/selector_automation/register_page.spec.ts
@@ -1,14 +1,23 @@
+// Playwright
+/* 
+Viết code automation cho test case sau:
+- Đi tới trang: https://material.playwrightvn.com/
+- Click vào: Bài học 1: Register Page (có đủ các element)
+- Điền vào username, email. Click button register.
+- Kiểm tra kết quả có chứa username và email tương ứng
+*/
+
 import { test, expect } from '@playwright/test';
 import { ENV } from '../../utils/env.config';
 
-test('Register Page', async ({ page }) => {
+test('Register Page_1', async ({ page }) => {
   await page.goto(ENV.BASE_URL);
   await page.getByRole('link', { name: /Register Page/ }).click();
 
   const username = ENV.TEST_USER_NAME;
   const email = ENV.TEST_USER_EMAIL;
   await page.waitForTimeout(ENV.SLOW_MO);
-  await page.getByLabel('Username').fill(username);``
+  await page.getByLabel('Username').fill(username);
   await page.waitForTimeout(ENV.SLOW_MO);
 
   await page.getByLabel('Email').fill(email);
@@ -21,4 +30,57 @@ test('Register Page', async ({ page }) => {
   await page.waitForTimeout(ENV.SLOW_MO);
   await expect(page.locator('//tbody//td').nth(1)).toHaveText(username);
   await expect(page.locator('//tbody//td').nth(2)).toHaveText(email);
+});
+
+/* 
+Viết code automation cho test case sau:
+- Đi tới trang: https://material.playwrightvn.com/
+- Click vào: Bài học 1: Register Page (có đủ các element)
+- Điền vào đầy đủ các thông tin của user
+- Kiểm tra kết quả đúng như thông tin đã điền.
+*/
+
+test('Register Page_2', async ({ page }) => {
+  await page.goto(ENV.BASE_URL);
+  await page.getByRole('link', { name: /Register Page/ }).click();
+
+  const username = ENV.TEST_USER_NAME;
+  const email = ENV.TEST_USER_EMAIL;
+
+  await page.getByLabel('Username').fill(username);
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByLabel('Email').fill(email);
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByRole('radio', { name: 'Male', exact: true }).click();
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByRole('checkbox', { name: 'Traveling' }).click();
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.locator('option:has-text("Music")').click();
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByRole('combobox', { name: 'Country:' }).selectOption('Canada');
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByLabel('Date of Birth:').fill('1990-01-01');
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByLabel('Biography:').fill('GenZ no^i? loan.');
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.waitForTimeout(ENV.SLOW_MO);
+
+  await expect(page.locator("//tbody//tr")).toHaveCount(1);
+  await page.waitForTimeout(ENV.SLOW_MO);
+  await expect(page.locator('tr').locator('td').nth(1)).toHaveText(username);
+  await expect(page.locator('tr').locator('td').nth(2)).toHaveText(email);
+  await expect(page.locator('tr').locator('td').nth(3)).toContainText('Gender: male');
+  await expect(page.locator('tr').locator('td').nth(3)).toContainText('Hobbies: traveling');
+  await expect(page.locator('tr').locator('td').nth(3)).toContainText('Country: canada');
+  await expect(page.locator('tr').locator('td').nth(3)).toContainText('Date of Birth: 1990-01-01');
+  await expect(page.locator('tr').locator('td').nth(3)).toContainText('Biography: GenZ no^i? loan.');
 });
